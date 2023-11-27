fea983e69e56a7246b1a47e14108c65ae155439e
Jason Andres López <117871599+J4ckson95@users.noreply.github.com>
2023-11-27 13:24:36 -0500

    Adelanto query params de products (#1)

--- src/router/products.router.js
+++ src/router/products.router.js
@@ -4,11 +4,15 @@ import productsModel from "../models/products.js"
 const router = Router()
 
 router.get("/", async (req, res) => {
-    const limit = parseInt(req.params)
-    const result = await productsModel.paginate({}, {
-        limit: 2, page: 1, lean: true
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const query = req.query.query || {};
+    let sort = req.query.sort || "";
+    if (sort != "" && sort == "asc") sort = 1
+    if (sort != "" && sort == "desc") sort = -1
+    const result = await productsModel.paginate(query, {
+        limit, page, sort: { price: sort }, lean: true
     })
-    console.log(result);
     res.render("showProducts", result)
 })
 router.post("/", async (req, res) => {

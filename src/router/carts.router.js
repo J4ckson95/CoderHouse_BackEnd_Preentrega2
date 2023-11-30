a1522e6ec4cd277cc394ff494466b7b7d4ef2306
Jason López <j4ckson95@gmail.com>
2023-11-29 21:40:30 -0500

    Adelanto carts

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -1,6 +1,14 @@
 import { Router } from "express"
 import cartsModel from "../models/carts.js"
 const router = Router()
+const updateCart = (cart, productId) => {
+    const indexProduct = cart.products.findIndex(element => element._id.toString() === productId);
+    if (indexProduct < 0) {
+        cart.products.push({ _id: productId, quantity: 1 });
+    } else {
+        cart.products[indexProduct].quantity += 1;
+    }
+}
 
 router.get("/:cid", async (req, res) => {
     try {
@@ -9,13 +17,14 @@ router.get("/:cid", async (req, res) => {
         res.render("showCarts", result)
     } catch (e) { res.status(500).send({ status: "Error", message: e.message }); }
 })
-router.get("/product/:idp", async (req, res) => {
+router.post("/product/:idp", async (req, res) => {
     try {
         const productId = req.params.idp
         let cart = await cartsModel.findOne()
-        if (!cart) cart = await cartsModel.create()
-        console.log(cart, productId);
-        res.send(cart)
+        if (!cart) cart = await cartsModel.create({});
+        updateCart(cart, productId)
+        const result = await cartsModel.updateOne({ _id: cart._id }, { $set: { products: cart.products } })
+        return res.send({ status: "Success", result })
     } catch (e) { res.status(500).send({ status: "Error", message: e.message }); }
 })
 

2b2bfe0ae5027d0307c56e7dac38a821dce73343
Jason <j4ckson95@gmail.com>
2023-11-30 13:20:14 -0500

    Adelanto oficina, sin funcionar showCarts

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -1,20 +1,17 @@
 import { Router } from "express"
 import cartsModel from "../models/carts.js"
 const router = Router()
-const updateCart = (cart, productId) => {
-    const indexProduct = cart.products.findIndex(element => element._id.toString() === productId);
-    if (indexProduct < 0) {
-        cart.products.push({ _id: productId, quantity: 1 });
-    } else {
-        cart.products[indexProduct].quantity += 1;
-    }
-}
 
 router.get("/:cid", async (req, res) => {
     try {
         const cartId = req.params.cid
-        const result = await cartsModel.findOne({ _id: cartId })
-        res.render("showCarts", result)
+        const result = await cartsModel.find({ _id: cartId })
+        //console.log();
+        const data = result.map((element) => {
+            title: element.products.product.title
+        })
+        console.log(data);
+        res.render("showCarts", { result })
     } catch (e) { res.status(500).send({ status: "Error", message: e.message }); }
 })
 router.post("/product/:idp", async (req, res) => {
@@ -28,4 +25,12 @@ router.post("/product/:idp", async (req, res) => {
     } catch (e) { res.status(500).send({ status: "Error", message: e.message }); }
 })
 
+const updateCart = (cart, productId) => {
+    const indexProduct = cart.products.findIndex(element => element.product.toString() === productId);
+    if (indexProduct < 0) {
+        cart.products.push({ product: productId, quantity: 1 });
+    } else {
+        cart.products[indexProduct].quantity += 1;
+    }
+}
 export default router
